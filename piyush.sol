89ef64b1310c170078ae237a2c06d26d32738cd0
piyush9444 <149815434+piyush9444@users.noreply.github.com>
2023-11-15 11:25:49 +0530

    Update piyush.sol

--- piyush.sol
+++ piyush.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.7;
 contract MyToken {
 
     // public variables here
-    string public Token Name = "PIYUSH9444";
-    string public Token Symbol = "PIYUSH";
-    uint public Token Supply = 0;
+    string public TokenName = "PIYUSH9444";
+    string public TokenSymbol = "PIYUSH";
+    uint public TokenSupply = 0;
 
     // mapping variable here
     mapping (address => uint) public balance;
 
     // mint function here
     function mint(address _address, uint _value) public {
-      tokenSupply += _value;
+      TokenSupply += _value;
       balance[_address] += _value;
     }
 
     // burn function here
     function burn(address _address, uint _value) public {
        if (balance[_address] >= _value) {
-          tokenSupply -= _value;
+          TokenSupply -= _value;
          balance[_address] -= _value;
       } 
     }
